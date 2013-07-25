a7f45a3dcdf89f5940e7958d8b2d21ff2a40e515
Sekou KOITA <sictoz@gmail.com>
2013-07-25 17:41:48 +0200

    Add api client

--- HttpClient/DaApiClient/RestHttpApiClient.php
+++ HttpClient/DaApiClient/RestHttpApiClient.php
@@ -10,8 +10,17 @@
 
 namespace IDCI\Bundle\NotificationApiClientBundle\HttpClient\DaApiClient;
 
+use Da\ApiClientBundle\HttpClient\RestApiClientBridge;
 use IDCI\Bundle\NotificationApiClientBundle\HttpClient\RestHttpApiClientInterface;
 
-class RestHttpApiClient extends DaApiClient implements RestHttpApiClientInterface
+class RestHttpApiClient extends RestApiClientBridge implements RestHttpApiClientInterface
 {
+    public function getNotif($offset, $limit)
+    {
+        $parameters = array('offset' => $offset, 'limit' => $limit);
+
+        $this->get('/friends', $parameters);
+
+        return 'It works!';
+    }
 }
