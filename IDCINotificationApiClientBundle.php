d7a5194994f88f6259165ad8ef5078fb8e49f265
Sekou KOITA <sictoz@gmail.com>
2013-07-25 16:55:14 +0200

    Add missing bundle loader class

---
+++ IDCINotificationApiClientBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Sekou KOÏTA <sekou.koita@supinfo.com>
+ * @license: GPL
+ *
+ */
+
+namespace IDCI\Bundle\NotificationApiClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCINotificationApiClientBundle extends Bundle
+{
+}
